79cd91269339e11b4ef988604620ab976c5fef46
xiaowuhu <xiaowuhu@microsoft.com>
2024-06-17 14:40:28 +0800

    Update backward_function_test.py

--- tests/function_libs/torch_lib/backward_function_test.py
+++ tests/function_libs/torch_lib/backward_function_test.py
@@ -245,8 +245,8 @@ class TestOperatorsOnnxrt(unittest.TestCase):
         training=None,
         input_index=None,
     ):
-        if sys.platform == "win32":
-            raise unittest.SkipTest("Windows not supported yet.")
+        # if sys.platform == "win32":
+        #     raise unittest.SkipTest("Windows not supported yet.")
         assert isinstance(onnx_export, str), f"Export onnx is wrong for f={f}"
         if isinstance(args, torch.Tensor):
             args = [args]
